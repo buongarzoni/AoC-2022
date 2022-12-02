b4d1a1e7444f88e0a5fdf8736394953d331a30a4
buongarzoni <christian.buongarzoni@gmail.com>
2022-12-02 02:17:24 -0300

    day02 solved, but at what cost?

--- src/App.kt
+++ src/App.kt
@@ -1,3 +1,3 @@
 fun main() {
-    solveDay01()
+    solveDay02()
 }
--- src/Day02.kt
+++ src/Day02.kt
@@ -1,3 +1,43 @@
 fun solveDay02() {
+    val input = readInput("Day02").map { ElveMatch.parse(it) }
+    println(input.sumOf { it.points })
+}
+
+data class ElveMatch(val first: String, val second: String) {
+    val points = getResult() + pointsForElection()
+    companion object {
+        fun parse(string: String) = ElveMatch(
+            first = string.take(1),
+            second = string.drop(2),
+        )
+    }
+
+    private fun getResult() = when(first) {
+        "A" -> when(second) {
+            "X" -> 3
+            "Y" -> 6
+            else -> 0
+        }
+        "B" -> when(second) {
+            "X" -> 0
+            "Y" -> 3
+            else -> 6
+        }
+        else -> when(second) {
+            "X" -> 6
+            "Y" -> 0
+            else -> 3
+        }
+    }
+
+    private fun pointsForElection() = when(second) {
+        "X" -> 1
+        "Y" -> 2
+        else -> 3
+    }
 
 }
+//A X rock
+//B Y paper
+//C z Scissor
+
