3f42c0aa0c2e3e8302b3ab8d5d9fe0c5cd8afc4f
buongarzoni <christian.buongarzoni@gmail.com>
2022-12-02 02:38:41 -0300

    day02 ok now Uncle Bob is crying

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,8 @@
 fun solveDay02() {
     val input = readInput("Day02").map { ElveMatch.parse(it) }
     println(input.sumOf { it.points })
+    val input2 = readInput("Day02").map { NewElveMatch.parse(it) }
+    println(input2.sumOf { it.points })
 }
 
 data class ElveMatch(val first: String, val second: String) {
@@ -37,7 +39,39 @@ data class ElveMatch(val first: String, val second: String) {
     }
 
 }
-//A X rock
-//B Y paper
-//C z Scissor
+
+data class NewElveMatch(val first: String, val second: String) {
+    val points = getResult() + pointsForElection()
+    companion object {
+        fun parse(string: String) = NewElveMatch(
+            first = string.take(1),
+            second = string.drop(2),
+        )
+    }
+
+    private fun getResult() = when(second) {
+        "X" -> 0
+        "Y" -> 3
+        else -> 6
+    }
+
+    private fun pointsForElection() = when(first) {
+        "A" -> when(second) {
+            "X" -> 3
+            "Y" -> 1
+            else -> 2
+        }
+        "B" -> when(second) {
+            "X" -> 1
+            "Y" -> 2
+            else -> 3
+        }
+        else -> when(second) {
+            "X" -> 2
+            "Y" -> 3
+            else -> 1
+        }
+    }
+}
+
 
