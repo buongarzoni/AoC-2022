a37df20b1e723c267872a20b8a1ab4f217d07ace
buongarzoni <christian.buongarzoni@gmail.com>
2022-12-03 02:24:44 -0300

    day03 part2

--- src/Day03.kt
+++ src/Day03.kt
@@ -2,6 +2,24 @@ fun solveDay03() {
     val result = readInput("Day03").mapNotNull { it.getItem() }.sum()
     println(result)
 
+    val result2 = readInput("Day03")
+        .chunked(3)
+        .mapNotNull { it.getChunkPoints() }
+        .sum()
+    println(result2)
+}
+
+private fun List<String>.getChunkPoints(): Int? {
+    for (character in component1()) {
+        if (component2().contains(character) && component3().contains(character)) {
+            return if (character.isLowerCase()) {
+                character - 'a' + 1
+            } else {
+                character - 'A' + 27
+            }
+        }
+    }
+    return null
 }
 
 private fun String.getItem(): Int? {
