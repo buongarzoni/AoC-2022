30b62deced289a501971309ee205f7f5f1397147
buongarzoni <christian.buongarzoni@gmail.com>
2022-12-04 02:09:58 -0300

    day04 part1

--- src/App.kt
+++ src/App.kt
@@ -1,3 +1,3 @@
 fun main() {
-    solveDay03()
+    solveDay04()
 }
---
+++ src/Day04.kt
@@ -0,0 +1,21 @@
+fun solveDay04() {
+    val result = readInput("Day04").map {line ->
+        line.split(',')
+    }.map {
+        val (first, second) = it
+        if (
+            first.range().first in second.range() && first.range().last in second.range() ||
+            second.range().first in first.range() && second.range().last in first.range()
+        ) {
+            1
+        } else {
+            0
+        }
+    }.sum()
+    println(result)
+}
+
+private fun String.range(): IntRange {
+    val (first, last) = split('-')
+    return first.toInt() .. last.toInt()
+}
