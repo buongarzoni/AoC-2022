4b30ed97b25f0589cbd56c8ef4f610ee250f0e7a
buongarzoni <christian.buongarzoni@gmail.com>
2022-12-04 04:06:50 -0300

    refactor day04 optimal

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,20 +1,28 @@
 fun solveDay04() {
-    val part1 = readInput("Day04")
+    val values = readInput("Day04")
+        .map { line -> line.split("-", ",").map { it.toInt() } }
+    val part1 = values.count { (a, b, c, d) -> (a <= c && b >= d) || (a >= c && b <= d) }
+    println(part1)
+    val part2 = values.count { (a, b, c, d) -> (c in a..b) || (a in c..d) }
+    println(part2)
+
+    val myPart1 = readInput("Day04")
         .count {
             val (firstRange, secondRange) = it.getRanges()
             firstRange in secondRange || secondRange in firstRange
         }
-    println(part1)
-    val part2 = readInput("Day04")
+    println(myPart1)
+    val myPart2 = readInput("Day04")
         .count {
             val (firstRange, secondRange) = it.getRanges()
             firstRange overlaps secondRange
         }
-    println(part2)
+    println(myPart2)
 }
 
 private operator fun IntRange.contains(other: IntRange) = first in other && last in other
-private infix fun IntRange.overlaps(other: IntRange) = first in other || last in other || other.first in this || other.last in this
+private infix fun IntRange.overlaps(other: IntRange) =
+    first in other || last in other || other.first in this || other.last in this
 
 private val String.lowLimit get(): Int = substringBefore('-').toInt()
 private val String.upperLimit get(): Int = substringAfter('-').toInt()
