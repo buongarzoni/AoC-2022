6c200b464f81112ad3b832dfd43792e185390825
buongarzoni <christian.buongarzoni@gmail.com>
2022-12-06 03:05:54 -0300

    day06 part2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,23 +1,22 @@
 fun solveDay06() {
     val input = readInput("Day06")
     solvePart1(input)
+    solvePart2(input)
 }
 
 private fun solvePart1(input: List<String>) {
-    input.map {
-        for (i in 0 ..it.length-4) {
-            val substring = it.substring(i, i + 4)
-            //println(substring)
-            if (
-                !substring.drop(1).contains(substring[0]) &&
-                !substring.drop(2).contains(substring[1]) &&
-                !substring.drop(3).contains(substring[2])
-            ) {
-                println(substring)
-                println(i+4)
-                break
-            }
-
-        }
+    input.forEach { line ->
+        val result = line.charactersUntilUniqueStringOfSize(4)
+        println("CRB solution part 1 : $result")
     }
 }
+
+private fun solvePart2(input: List<String>) {
+    input.forEach { line ->
+        val result = line.charactersUntilUniqueStringOfSize(14)
+        println("CRB solution part 2 : $result")
+    }
+}
+
+private fun String.charactersUntilUniqueStringOfSize(size: Int) = windowed(size)
+    .indexOfFirst { it.toSet().size == size } + size
