bc702385c6a9de2c1f621fc6a6b22f4960e1ac2c
buongarzoni <christian.buongarzoni@gmail.com>
2022-12-10 03:41:46 -0300

    day11 prep

--- src/App.kt
+++ src/App.kt
@@ -1,3 +1,3 @@
 fun main() {
-    solveDay10()
+    solveDay11()
 }
---
+++ src/Day11.kt
@@ -0,0 +1,8 @@
+fun solveDay11() {
+    val input = readInput("Day11_test")
+    solve(input)
+}
+
+private fun solve(input: List<String>) {
+
+}
