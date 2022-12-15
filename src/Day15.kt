96aadef37d79bcd9880dbc540e36984fb0f83ce0
buongarzoni <christian.buongarzoni@gmail.com>
2022-12-15 04:55:31 -0300

    day15 part2

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,9 +1,10 @@
 import kotlin.math.abs
+import kotlin.math.absoluteValue
 import kotlin.math.max
 import kotlin.math.min
 
 private const val ROW = 2_000_000
-
+private const val TUNING_FREQUENCY_MULTIPLIER = 4_000_000
 fun solveDay15() {
     val lines = readInput("Day15")
     solve(lines)
@@ -16,28 +17,41 @@ var maxY = 0
 private fun solve(
     lines: List<String>,
 ) {
+    val sensorsRadius = mutableMapOf<Position, Int>()
     val sensors = mutableSetOf<Position>()
     val beacons = mutableSetOf<Position>()
-    val cantExist = mutableSetOf<Position>()
+    val invalidPositions = mutableSetOf<Position>()
 
     lines.forEach { line ->
         val sensor = line.getSensor()
         val beacon = line.getBeacon()
-        if (sensor.y == ROW) {
-            sensors.add(sensor)
-        }
-        if (beacon.y == ROW) {
-            beacons.add(beacon)
-        }
+        if (sensor.y == ROW) sensors.add(sensor)
+        if (beacon.y == ROW) beacons.add(beacon)
+
         val distance = sensor distance beacon
+        sensorsRadius[sensor] = distance +1
         if (ROW in (sensor.y - distance)..(sensor.y + distance)) {
             for (x in (sensor.x - distance + abs(sensor.y - ROW))..(sensor.x + distance - abs(sensor.y - ROW))) {
-                cantExist.add(Position(x, ROW))
+                invalidPositions.add(Position(x, ROW))
             }
         }
     }
+    val part1 = invalidPositions.size - beacons.size - sensors.size
+    println("Solution part 1 : $part1")
 
-    println(cantExist.size - beacons.size - sensors.size)
+    val range = 0 .. TUNING_FREQUENCY_MULTIPLIER
+    val distress = sensorsRadius.flatMap { (sensor, distance) ->
+        (-distance .. distance).flatMap { xDelta ->
+            val yDelta = distance -xDelta.absoluteValue
+            listOf(
+                Position(sensor.x + xDelta, sensor.y - yDelta),
+                Position(sensor.x + xDelta, sensor.y + yDelta),
+            ).filter { (it.x in range && (it.y in range)) }
+        }
+    }.first { point -> sensorsRadius.all { it.key distance point >= it.value } }
+
+    val part2 = distress.x.toLong() * TUNING_FREQUENCY_MULTIPLIER + distress.y
+    println("Solution part 2 : $part2")
 
 }
 
